9cfdc44b156eb92a44065552470696342fd9b3e7
meejah <meejah@meejah.ca>
2015-03-20 17:44:21 -0600

    mad haxxoring to make feeding msgpack buffer work

--- client/client.cpp
+++ client/client.cpp
@@ -27,26 +27,45 @@ class chat_message
 {
 public:
     chat_message() {}
-    
+
     std::string nickname;
     std::string msg;
-    
+
     MSGPACK_DEFINE( nickname, msg );
 };
 
 typedef std::deque<chat_message> chat_message_queue;
 
+typedef boost::asio::buffers_iterator<boost::asio::streambuf::const_buffers_type> iterator;
+std::pair<iterator, bool>match(iterator begin, iterator end)
+{
+    int size = end - begin;
+    // XXX FIXME
+    msgpack::unpacker unpacker_;
+    unpacker_.reserve_buffer(size);
+    std::copy(begin, end, unpacker_.buffer());
+    unpacker_.buffer_consumed(size);
+
+    msgpack::unpacked result;
+    bool got_one = unpacker_.next(&result);
+    return std::make_pair(end, got_one);
+};
+
+
 class chat_client
 {
+    msgpack::unpacker unpacker_;
+
 public:
     chat_client( boost::asio::io_service& io_service,
                  tcp::resolver::iterator endpoint_iterator )
         : io_service_( io_service ),
-          socket_( io_service )
+          socket_( io_service ),
+          unpacker_()
     {
         do_connect( endpoint_iterator );
     }
-    
+
     void write( const chat_message& msg )
     {
         io_service_.post(
@@ -54,16 +73,17 @@ public:
         {
             msgpack::sbuffer sbuf;
             msgpack::pack( sbuf, msg );
-            memcpy( write_msg_.data(), sbuf.data(), sbuf.size() );
+            //memcpy( write_msg_.data(), sbuf.data(), sbuf.size() );
+            std::copy(sbuf.data(), sbuf.data() + sbuf.size(), write_msg_.data() );
             do_write( sbuf.size() );
         } );
     }
-    
+
     void close()
     {
         io_service_.post( [this]() { socket_.close(); } );
     }
-    
+
 private:
     void do_connect( tcp::resolver::iterator endpoint_iterator )
     {
@@ -76,63 +96,48 @@ private:
             }
         } );
     }
-    
+
     void do_read_header()
     {
-        boost::asio::async_read( socket_,
-                                 boost::asio::buffer( read_msg_.data(), 1 ),
-                                 [this]( boost::system::error_code ec, std::size_t /*length*/ )
-        {
-            unsigned msg_length = ( unsigned )read_msg_[0];
-            //cout << "incoming msg will be " << msg_length << " bytes";
-            
-            if( ec )
-            {
-                socket_.close();
-                return;
-            }
-            
-            
-            do_read_body( msg_length );
-        } );
+        do_read_body();
     }
-    
-    void do_read_body( unsigned msg_length )
+
+
+    void do_read_body()
     {
-        boost::asio::async_read( socket_,
-                                 boost::asio::buffer( read_msg_.data(), msg_length ),
-                                 [this]( boost::system::error_code ec, std::size_t length )
-        {
-            if( ec )
+        boost::asio::streambuf buffer;
+
+        socket_.async_read_some(
+            boost::asio::buffer( read_msg_.data(), 1024 ),
+            [this]( boost::system::error_code ec, std::size_t length )
             {
-                socket_.close();
-                return;
-            }
-            
-            try
-            {
-                msgpack::zone zone;
-                msgpack::object obj;
-                msgpack::unpack( read_msg_.data(), length, NULL, &zone, &obj );
-                obj.convert( &msg );
-            }
-            catch( std::bad_cast& e )
-            {
-                cerr << "recv'd bad message, closing connection";
-                socket_.close();
-                return;
-            }
-            
-            cout << endl << msg.nickname << ": " << msg.msg << endl;
-            do_read_header();
-        });
+                if( ec )
+                {
+                    socket_.close();
+                    return;
+                }
+
+                // feed data to unpacker
+                unpacker_.reserve_buffer(length);
+                std::copy(read_msg_.data(), read_msg_.data() + length,
+                          unpacker_.buffer());
+                unpacker_.buffer_consumed(length);
+
+                // maybe-get object out of it
+                msgpack::unpacked result;
+                if (unpacker_.next(&result)) {
+                    result.get().convert(&msg);
+                    cout << endl << msg.nickname << ": " << msg.msg << endl;
+                }
+                do_read_header();
+            });
     }
-    
+
     void do_write( unsigned msg_length )
     {
-        std::array<char, 1> b = { ( char )msg_length };
-        boost::asio::write( socket_, boost::asio::buffer( b, 1 ) );
-        
+//        std::array<char, 1> b = { ( char )msg_length };
+//        boost::asio::write( socket_, boost::asio::buffer( b, 1 ) );
+
         boost::asio::async_write( socket_,
                                   boost::asio::buffer( write_msg_.data(), msg_length ),
                                   [this]( boost::system::error_code ec, std::size_t length )
@@ -143,15 +148,15 @@ private:
                 socket_.close();
                 return;
             }
-            
+
             //std::cout << "sent " << length << endl;
         } );
     }
-    
+
 private:
     boost::asio::io_service& io_service_;
     tcp::socket socket_;
-    
+
     buffer_t read_msg_;
     buffer_t write_msg_;
     chat_message msg;
@@ -166,33 +171,33 @@ int main( int argc, char* argv[] )
             std::cerr << "Usage: chat_client <nickname> <host> <port>\n";
             return 1;
         }
-        
+
         boost::asio::io_service io_service;
-        
+
         tcp::resolver resolver( io_service );
         auto endpoint_iterator = resolver.resolve( { argv[2], argv[3] } );
         chat_client c( io_service, endpoint_iterator );
-        
+
         std::thread t( [&io_service]() { io_service.run(); } );
-        
+
         char line[256];
-        
+
         cout << argv[1] << ": " << std::flush;
-        
+
         while( std::cin.getline( line, 255 ) )
         {
             chat_message msg;
             msg.nickname = argv[1];
-            
+
             std::string l = line;
             l.erase( std::remove( l.begin(), l.end(), '\n' ), l.end() );
             msg.msg = l;
-            
+
             c.write( msg );
 
             cout << argv[1] << ": " << std::flush;
         }
-        
+
         c.close();
         t.join();
     }
@@ -200,7 +205,6 @@ int main( int argc, char* argv[] )
     {
         std::cerr << "Exception: " << e.what() << "\n";
     }
-    
+
     return 0;
 }
-
